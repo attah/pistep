43083b1488827a9b93f126b0338d80e14e1ff379
attah <antonthomasson@gmail.com>
2016-10-09 13:49:53 +0200

    Update README.mk
    step-by-step

--- README.mk
+++ README.mk
@@ -1,1 +1,8 @@
 Hey! Look at my shitty code. :)
+
+1. make
+2. ./_rel/pistep_release/bin/pistep_release console
+3. pistep:readFile("/path/to/some/gcode.ngc"). 
+4. steps ! done.
+5. open browser to 127.0.0.1:8080
+6. steps ! run.
